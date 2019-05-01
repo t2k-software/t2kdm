1f051f1ba4eab03f2f87b4201c943aaec930f129
Lukas Koch <lukas.koch@stfc.ac.uk>
2019-05-01 18:19:24 +0100

    Post-release version bump.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ experiment.
 """
 
 setup(name='t2kdm',
-    version='1.5.0',
+    version='1.5.1-dev',
     description=description,
     long_description=long_description,
     url='https://github.com/t2k-software/t2kdm',
