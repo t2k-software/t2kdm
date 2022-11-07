bd96a813d642e805cff742f78319fe76db37701f
Tomislav Vladisavljevic <vladisavljevic.tomislav@gmail.com>
2022-11-07 15:15:47 +0000

    Fix Python 3 TypeError in t2kdm-maid.

--- t2kdm/maid.py
+++ t2kdm/maid.py
@@ -189,7 +189,7 @@ class Task(object):
 
     def get_logname(self):
         """Get a valid filename that can be used to log the output of the task."""
-        return base64.b64encode(self.get_id(), altchars="+_") + ".txt"
+        return base64.b64encode(str.encode(self.get_id()), altchars="+_") + ".txt"
 
     def get_id(self):
         """Return a string that identifies the task."""
